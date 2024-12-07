a6d7302a23d2371e1624b18cbff92b8916a1018b
Hai-VanNgo <ngovanhai120799@gmail.com>
2024-12-07 13:18:27 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
+
+from models import setup_db
 
 app = Flask(__name__)
+CORS(app)
+def create_app():
+    """Application-factory pattern"""
+    from api.actor import actor_api
+    app.register_blueprint(actor_api)
 
-@app.route("/hello", methods=["GET"])
-def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+    from api.movie import movie_api
+    app.register_blueprint(movie_api)
+
+    from api.auth import auth
+    app.register_blueprint(auth)
+    setup_db(app)
+    return app
